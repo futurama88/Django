375cce5a8a391b483c349af9d8acec5fe262ec69
futurama88 <147860808+futurama88@users.noreply.github.com>
2024-01-07 21:51:24 +0300

    Update views.py

--- D5/accounts/views.py
+++ D5/accounts/views.py
@@ -2,9 +2,19 @@ from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
 from .forms import SignUpForm
 
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+
 
 class SignUp(CreateView):
     model = User
     form_class = SignUpForm
     success_url = '/accounts/login'
     template_name = 'registration/signup.html'
+
+class CustomSignupForm(SignupForm):
+    def save(self, request):
+        user = super().save(request)
+        common_users = Group.objects.get(name="common users")
+        user.groups.add(common_users)
+        return user
