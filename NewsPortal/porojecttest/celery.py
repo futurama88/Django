ee518e162889a7b84710a98571046674d11e4fe5
futurama88 <147860808+futurama88@users.noreply.github.com>
2024-01-28 21:36:07 +0300

    Update celery.py

--- NewsPortal/porojecttest/celery.py
+++ NewsPortal/porojecttest/celery.py
@@ -11,7 +11,7 @@ app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'action_every_monday_8am': {
-        'task': 'NewsPortal.tasks.weekly_send_email_task',
+        'task': 'News.tasks.weekly_send_email_task',
         'schedule': crontab(),  # hour=8, minute=0, day_of_week='monday'
         'args': ()
     }
