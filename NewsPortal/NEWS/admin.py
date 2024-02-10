5515097af530c322d15ede72811e59d2cc116d3c
Кирилл <147860808+futurama88@users.noreply.github.com>
2024-02-10 11:38:47 +0300

    Update admin.py

--- NewsPortal/NEWS/admin.py
+++ NewsPortal/NEWS/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import Category, Product, Author, Post, PostCategory, Comment
 
+
+def nullfy_rating(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+    nullfy_rating.short_description = 'Обнулить рейтинг'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'dataCreation', 'rating', 'categoryType') 
+    list_filter = ('author', 'rating', 'categoryType')  
+    search_fields = ('name', 'category__name')  
+    actions = [nullfy_rating]  
+
+
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
+# admin.site.unregister(Product)
