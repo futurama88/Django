5ab7f1f89cf6041bc8b3478fe59f0bf025c9f55b
Кирилл <147860808+futurama88@users.noreply.github.com>
2024-03-04 09:52:08 +0300

    Update settings.py

--- NewsPortal/projecttest/settings.py
+++ NewsPortal/projecttest/settings.py
@@ -288,10 +288,10 @@ LOGGING = {
     'filters': {
         'require_debug_true': {
             '()': 'django.utils.log.RequireDebugTrue',
+          },
+          'require_debug_false': {
+              '()': 'django.utils.log.RequireDebugFalse',
          },
-        # 'require_debug_false': {
-        #     '()': 'django.utils.log.RequireDebugFalse',
-        #},
     },
 }
 
