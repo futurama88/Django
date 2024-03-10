62c467b851fb37314fadf8dde0e99ba5d2e2401f
Кирилл <147860808+futurama88@users.noreply.github.com>
2024-03-10 09:29:44 +0300

    Add files via upload

---
+++ NewsPortal/NEWS/translation.py
@@ -0,0 +1,14 @@
+from .models import Category, Post
+from modeltranslation.translator import register, TranslationOptions
+
+
+# регистрируем наши модели для перевода
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('name',)  # указываем, какие именно поля надо переводить в виде кортежа
+
+
+@register(Post)
+class PostTranslationOptions(TranslationOptions):
+    fields = ('title', 'rating', 'text',)
